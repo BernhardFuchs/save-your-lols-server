514e23b40ceb1bbfccf7a138728f8b9c9dbcf7e7
bernhardfuchs <bernifox82@gmail.com>
2018-02-25 10:32:06 +0100

    redirection blocked by client

--- src/server-app.ts
+++ src/server-app.ts
@@ -1,7 +1,11 @@
 import * as express from 'express';
 import {Application, Request, Response} from 'express';
+import * as http from 'http';
 
 import {TclController} from './tcl-controller'
+import { IncomingMessage } from 'http';
+
+// use internal http.get to fetch data
 
 class ServerApp {
     
@@ -24,11 +28,49 @@ class ServerApp {
         });
         
         const randomPath: string | RegExp | (string | RegExp)[] = '/random';
-        this.app.route(randomPath).get((req: Request, res: Response) => {
+        this.app.route(randomPath).get((req: Request, resToClient: Response) => {
             console.log(`Random Path: ${randomPath}`);
-            console.log(`Request: ${req}`);
-            console.log('No error returned!!!');
-            res.sendStatus(200);
+
+            const responseData = {
+                originUrl: '',
+                headline: '',
+                gifUrl: ''
+            }
+
+            http.get('http://thecodinglove.com/random/', (incMessage: IncomingMessage) => {
+                
+                const statusCode: number = incMessage.statusCode;
+                console.log('Status code incMessage: ', statusCode);
+                
+                if(statusCode >= 300 && statusCode < 400) {
+                    const location = incMessage.headers.location;
+                    console.log(`Redirecting to ${location}`);
+                    
+                    http.get(location, (incBody) => {
+                        console.log(`Redirection call with Status ${incBody.statusCode}`);
+                        console.log(`Headers of redirection Response ${incBody.headers.location}`);
+                        incBody.setEncoding('UTF-8');
+                        let body = '';
+
+                        incMessage.on('data', data => {
+                            console.log('Data is: ', typeof data);
+                            body += data;
+                        });
+
+                        incBody.on('end', () => {
+                            responseData.originUrl = location;
+                            responseData.headline = body;
+                            responseData.gifUrl = 'http://url.com';
+                            resToClient.json(responseData);
+                        });
+
+                    });
+                }
+
+                incMessage.on('end', () => {
+                    resToClient.status(500);
+                });
+            });
         });
     }
 
