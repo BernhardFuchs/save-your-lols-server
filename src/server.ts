18b9ceafd4ed2f93a80fd2e1e797821aa9aec841
bernhardfuchs <bernifox82@gmail.com>
2018-03-07 14:17:17 +0100

    - cleaning up before merge

--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,3 @@ const server = app.listen(app.get('port'), () => {
 });
 
 export default server;
-
-
-/* Class based solution
-import {ServerApp} from './server-app'
-
-const port = process.env.PORT || 3000
-
-const app = new ServerApp().app;
-app.listen(port, (err) => {
-  if (err) {
-    return console.log(err)
-  }
-
-  return console.log(`App is running at http://localhost:${port} 
-  in ${app.get('env')} mode\n` + '  Press CTRL-C to stop\n');
-});*/
